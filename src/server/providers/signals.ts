2e70c594526dfe963076a7db0c11af45422a6bda
coolergate <ejjiah233@gmail.com>
2022-11-29 17:34:31 -0300

    Update signals.ts

--- src/server/providers/signals.ts
+++ src/server/providers/signals.ts
@@ -6,10 +6,13 @@ const Signals = {
 	PlayerRemoved: new LocalSignal<[id: number, data: PlayerData]>(),
 	GetPlayerDataFromUserId: new LocalFunction<[UserId: number], PlayerData | undefined>(),
 
-	GetEntityFromUserId: new LocalFunction<[UserId: number, Entity: PlayerEntityInfo], PlayerEntityInfo | undefined>(),
-	PlayerEntityCreated: new LocalSignal<[Entity: PlayerEntityInfo]>(),
-	PlayerEntitySpawned: new LocalSignal<[Entity: PlayerEntityInfo]>(),
-	PlayerEntityDied: new LocalSignal<[Entity: PlayerEntityInfo]>(),
+	GetEntityFromUserId: new LocalFunction<
+		[UserId: number, Entity: PlayerEntityInfo_1],
+		PlayerEntityInfo_1 | undefined
+	>(),
+	PlayerEntityCreated: new LocalSignal<[Entity: PlayerEntityInfo_1]>(),
+	PlayerEntitySpawned: new LocalSignal<[Entity: PlayerEntityInfo_1]>(),
+	PlayerEntityDied: new LocalSignal<[Entity: PlayerEntityInfo_1]>(),
 };
 
 export = Signals;
