bf13aa3ca3d735f40c83efce2008d3d1ab0f21ea
coolergate <ejjiah233@gmail.com>
2022-12-05 02:37:29 -0300

    Removed old signals with deprecated interface

--- src/server/providers/signals.ts
+++ src/server/providers/signals.ts
@@ -4,14 +4,6 @@ const Signals = {
 	PlayerAdded: new LocalSignal<[id: number, data: PlayerData]>(),
 	PlayerRemoved: new LocalSignal<[id: number, data: PlayerData]>(),
 	GetPlayerDataFromUserId: new LocalFunction<[UserId: number], PlayerData | undefined>(),
-
-	GetEntityFromUserId: new LocalFunction<
-		[UserId: number, Entity: PlayerEntityInfo_1],
-		PlayerEntityInfo_1 | undefined
-	>(),
-	PlayerEntityCreated: new LocalSignal<[Entity: PlayerEntityInfo_1]>(),
-	PlayerEntitySpawned: new LocalSignal<[Entity: PlayerEntityInfo_1]>(),
-	PlayerEntityDied: new LocalSignal<[Entity: PlayerEntityInfo_1]>(),
 };
 
 export = Signals;
