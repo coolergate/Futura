64b777f53178a45436ba5ab66c02691edb3e5393
coolergate <ejjiah233@gmail.com>
2022-12-16 01:41:37 -0300

    create client-accessible commands

---
+++ src/server/providers/client_cmds.ts
@@ -0,0 +1,23 @@
+// Author: coolergate#2031
+// Purpose: Create commands that can be accessed via the client's console
+
+import Network = require('shared/network');
+
+export const created_commands = new Array<client_command<any>>();
+
+export class client_command<args extends string[]> {
+	name: string;
+	callback: (player: PlayerData_Advanced, ...args: args) => void | string;
+
+	constructor(name: string) {
+		this.name = name;
+
+		this.callback = (player, ...args) => {
+			print(player);
+			warn(...args);
+			return `server-side callback has not been defined! (${this.name})`;
+		};
+
+		created_commands.insert(0, this);
+	}
+}
