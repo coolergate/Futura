97487dc2a5d40f675b246bca65af32a7641f8a99
coolergate <ejjiah233@gmail.com>
2022-12-16 01:42:50 -0300

    Update values.ts

--- src/client/providers/values.ts
+++ src/client/providers/values.ts
@@ -1,25 +1,15 @@
-//========= Copyright GGC Studios, All rights reserved. ============//
-// Purpose: Store script values to be used with other scripts		//
+// Author: coolergate#2031
+// Purpose: Store script values to be used with other scripts
 //==================================================================//
 
 const Values = {
-	// character values
-	Character: {
-		Health: 0,
-		MaxHealth: 150,
-		CollisionBox: undefined as PlayerCollisionModel | undefined,
-		Id: '',
-	},
-
-	// Movement values
-	MovementInputRequest: false,
+	Character: undefined as ent_CharacterInfo | undefined,
 
 	// camera values
-	CCameraCFrame: new CFrame(),
-	CCameraDelta: new Vector2(),
-	CCameraEnable: true,
-	CCameraUnlock: new Map<string, boolean>(),
-	CCameraLockTo: undefined as BasePart | undefined,
+	camCFrame: new CFrame(),
+	camDelta: new Vector2(),
+	camEnable: true,
+	camUnlock: new Map<string, boolean>(),
 };
 
 export = Values;
