523b3d5ea10e35a7a2a80cfcdd1c8a2621d542b3
coolergate <ejjiah233@gmail.com>
2022-12-16 01:40:53 -0300

    Update signals.ts

--- src/server/providers/signals.ts
+++ src/server/providers/signals.ts
@@ -4,6 +4,10 @@ const Signals = {
 	PlayerAdded: new LocalSignal<[id: number, data: PlayerData]>(),
 	PlayerRemoved: new LocalSignal<[id: number, data: PlayerData]>(),
 	GetPlayerDataFromUserId: new LocalFunction<[UserId: number], PlayerData | undefined>(),
+
+	// new
+	get_id_from_player: new LocalFunction<[user: Player], number>(),
+	get_plrdata_from_id: new LocalFunction<[id: number], PlayerData | undefined>(),
 };
 
 export = Signals;
