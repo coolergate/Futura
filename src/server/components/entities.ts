5532adf18cdc74a815bcbc556bf7a440c1e5bf97
coolergate <ejjiah233@gmail.com>
2022-12-25 02:21:45 -0300

    character entity rework

--- src/server/components/entities.ts
+++ src/server/components/entities.ts
@@ -12,178 +12,249 @@ import Network from 'shared/network';
 import { client_command } from 'server/providers/client_cmds';
 
 // ANCHOR Folders
-const characters_folder = Folders.GetFolder('characters', Folders.Entities);
+const char_collision_folder = Folders.CharacterEntity.Collision;
+const HumanoidDescription_Folder = Folders.GetFolder('HumanoidDescriptions', Folders.Entities);
+
+interface BaseEntity {
+	base_id: string;
+	base_model?: Instance;
+
+	Remove(): void;
+	Create(): void;
+}
 
 //SECTION Character entity
-const created_controllers = new Array<CharacterController>();
+const created_CharacterControllers = new Array<CharacterController>();
 const respawn_req = new client_command<[], void>('char_respawn');
+const def_humanoid_desc = new Instance('HumanoidDescription', HumanoidDescription_Folder);
+def_humanoid_desc.Name = 'DefaultHumanoidDescription';
+
+//ANCHOR CVars
+const cvar_default_maxhealth = new CVar('char_starting_maxhealth', 150, '');
+const cvar_default_health = new CVar('char_starting_health', cvar_default_maxhealth.value, '');
 
 declare global {
-	interface ent_characterColl extends Part {
+	interface CharacterCollision extends Part {
 		CameraAttachment: Attachment;
 		MainAttachment: Attachment;
 	}
-	interface ent_CharacterInfo {
-		id: string;
-		alive: boolean;
-		health: number;
-		health_max: number;
+	interface CharacterInfoLocal {
+		Id: string;
+		Alive: boolean;
+		Health: number;
+		MaxHealth: number;
 
-		collisionbox: ent_characterColl;
+		CollisionBox: CharacterCollision;
 	}
+	interface info_entCharacter_replicated {
+		id: string;
+		skin_applied: string;
+		collisionbox: CharacterCollision;
+	}
+	type info_entCharacter_type = 'Local' | 'Replicated';
 }
 
+// ANCHOR CharacterController
 class CharacterController {
-	readonly ent_id = tostring(characters_folder.GetChildren().size() + 1); //GenerateString(math.random(10, 20));
-	readonly ent_collisionbox: ent_characterColl;
+	readonly id = tostring(char_collision_folder.GetChildren().size() + 1); //GenerateString(math.random(10, 20));
+	collisionbox: CharacterCollision;
 
-	ent_owner = undefined as number | undefined;
-	ent_alive = true;
-	ent_health = 150;
-	ent_maxhealth = 150;
+	private PInfo = {
+		collisionbox_name: Services.RunService.IsStudio()
+			? 'character_' + tostring(char_collision_folder.GetChildren().size() + 1)
+			: GenerateString(math.random(10, 20)),
+	};
+
+	Info = {
+		Controlling: undefined as PlayerMonitor | undefined,
+		Description: def_humanoid_desc as HumanoidDescription | undefined,
+
+		Alive: false,
+		Health: 0,
+		HealthMax: cvar_default_maxhealth.value,
+	};
 
 	// signals
 	signal_damaged = new LocalSignal<[amount: number]>();
-	signal_died = new LocalSignal<[UserId: number | undefined]>();
+	signal_died = new LocalSignal<[Controller: PlayerMonitor | undefined]>();
 
-	// functions
-	constructor() {
-		// create collision box
-		const collision_box = new Instance('Part', characters_folder) as ent_characterColl;
-		collision_box.Size = new Vector3(2, 5, 2);
-		collision_box.Transparency = 0.5;
-		collision_box.CustomPhysicalProperties = new PhysicalProperties(1, 1, 0, 0, 100);
-		collision_box.Anchored = true;
-		collision_box.CFrame = new CFrame(0, 10e8, 0);
-		collision_box.Name = Services.RunService.IsStudio()
-			? 'character_' + tostring(characters_folder.GetChildren().size() + 1)
-			: GenerateString(math.random(10, 20), '§');
+	private CreateCollisionBox() {
+		const cbox = new Instance('Part', char_collision_folder) as CharacterCollision;
+		cbox.Size = new Vector3(2, 5, 2);
+		cbox.Transparency = 0.5;
+		cbox.CustomPhysicalProperties = new PhysicalProperties(1, 1, 0, 0, 100);
+		cbox.Anchored = true;
+		cbox.CFrame = new CFrame(0, 10e8, 0);
+		cbox.Name = this.PInfo.collisionbox_name;
 
 		// vforce attachment
-		const attachment_1 = new Instance('Attachment', collision_box);
-		attachment_1.Name = 'MainAttachment';
+		const main_attach = new Instance('Attachment', cbox);
+		main_attach.Name = 'MainAttachment';
 
 		// camera attachment
-		const attachment_2 = new Instance('Attachment', collision_box);
-		attachment_2.Position = new Vector3(0, 2, 0);
-		attachment_2.Name = 'CameraAttachment';
+		const cam_attach = new Instance('Attachment', cbox);
+		cam_attach.Position = new Vector3(0, 2, 0);
+		cam_attach.Name = 'CameraAttachment';
 
-		this.ent_collisionbox = collision_box;
+		return cbox;
+	}
+
+	constructor() {
+		this.collisionbox = this.CreateCollisionBox();
+
+		// if the collision box gets deleted, we'll make sure to create a replacement
+		// probably will only happen when it falls of the world and gets deleted
+		this.collisionbox.Destroying.Connect(() => {
+			this.Kill();
+		});
 	}
 
 	Damage(amount: number) {
-		if (this.ent_owner === undefined) return;
-		this.ent_health = math.clamp(this.ent_health - amount, 0, this.ent_maxhealth);
-		this.ent_health <= 0 && this.ent_alive ? this.Kill() : this.signal_damaged.Fire(amount);
+		if (this.Info.Controlling === undefined) return;
+		this.Info.Health = math.clamp(this.Info.Health - amount, 0, this.Info.HealthMax);
+		this.Info.Health <= 0 && this.Info.Alive ? this.Kill() : this.signal_damaged.Fire(amount);
 	}
 
 	Kill() {
-		this.ent_alive = false;
-		this.ent_health = 0;
-		this.ent_collisionbox.Anchored = false;
-		this.ent_collisionbox.SetNetworkOwner();
-		this.ent_collisionbox.Anchored = true;
-		this.signal_died.Fire(this.ent_owner);
-		this.ent_owner = undefined;
+		this.Info.Alive = false;
+		this.Info.Health = 0;
+		this.signal_died.Fire(this.Info.Controlling);
+
+		this.Unassign();
+
+		this.collisionbox.Anchored = true;
+		this.collisionbox.CFrame = new CFrame(0, 10e8, 0);
 	}
 
-	Spawn(userid: number) {
-		this.ent_alive = true;
-		this.ent_health = this.ent_maxhealth;
-		this.ent_owner = userid;
+	Spawn() {
+		if (this.Info.Controlling === undefined || this.collisionbox.GetNetworkOwner() === undefined) {
+			warn('Spawn method requires one user controlling');
+			print('ControllerId:', this.id);
+			return;
+		}
+
+		this.collisionbox.Anchored = true;
+
+		// Random spawn location
+		const spawn_children = Folders.Map.func_spawn.GetChildren();
+		let Spawn_Location = new CFrame(0, 10, 0);
+		if (!spawn_children.isEmpty()) {
+			let Found = false;
+			do {
+				const result = spawn_children[math.random(0, spawn_children.size())];
+				if (!result || !result.IsA('BasePart')) continue;
+
+				Spawn_Location = result.CFrame;
+				Found = true;
+				break;
+			} while (Found === false);
+		}
+
+		this.collisionbox.CFrame = Spawn_Location;
+		this.collisionbox.Anchored = false;
+		if (this.Info.Controlling !== undefined) this.collisionbox.SetNetworkOwner(this.Info.Controlling.Instance);
+
+		this.Info.HealthMax = cvar_default_maxhealth.value;
+		this.Info.Health = math.clamp(cvar_default_health.value, 1, cvar_default_maxhealth.value);
+		this.Info.Alive = true;
+	}
+
+	Assign(PlayerInfo: PlayerMonitor) {
+		const description =
+			(HumanoidDescription_Folder.FindFirstChild(tostring(PlayerInfo.Instance)) as
+				| HumanoidDescription
+				| undefined) || Services.Players.GetHumanoidDescriptionFromUserId(PlayerInfo.Instance.UserId);
+		description.Parent = HumanoidDescription_Folder;
+		description.Name = tostring(PlayerInfo.UserId);
+
+		this.Info.Description = description;
+		this.Info.Controlling = PlayerInfo;
+		this.collisionbox.Anchored = false;
+		this.collisionbox.SetNetworkOwner(PlayerInfo.Instance);
+	}
+
+	Unassign() {
+		this.Info.Controlling = undefined;
+		this.Info.Description = def_humanoid_desc;
+		this.collisionbox.Anchored = false;
+		this.collisionbox.SetNetworkOwner();
 	}
 }
 
-function char_InfoFromController(controller: CharacterController): ent_CharacterInfo {
+function GenerateCharacterInfo(controller: CharacterController): CharacterInfoLocal {
 	return {
-		id: controller.ent_id,
-		alive: controller.ent_alive,
-		health: controller.ent_health,
-		health_max: controller.ent_maxhealth,
-		collisionbox: controller.ent_collisionbox,
+		Id: controller.id,
+		Alive: controller.Info.Alive,
+		Health: controller.Info.Health,
+		MaxHealth: controller.Info.HealthMax,
+		CollisionBox: controller.collisionbox,
 	};
 }
 
 // create 5 controllers above maxplayers
 for (let index = 0; index < Services.Players.MaxPlayers + 5; index++) {
-	const ent_player_controller = new CharacterController();
-	created_controllers.insert(0, ent_player_controller);
+	const Controller = new CharacterController();
+	created_CharacterControllers.insert(0, Controller);
 
-	ent_player_controller.signal_damaged.Connect(amount => {
-		if (ent_player_controller.ent_owner === undefined) return;
-		const controller_owner = Services.Players.GetPlayerByUserId(ent_player_controller.ent_owner);
-		if (!controller_owner) return;
+	Controller.signal_damaged.Connect(amount => {
+		if (Controller.Info.Controlling === undefined) return;
 
+		// send local info
 		Network.entities.ent_Character.info_changed.PostClient(
-			[controller_owner],
-			char_InfoFromController(ent_player_controller),
+			[Controller.Info.Controlling.Instance],
+			'Local',
+			GenerateCharacterInfo(Controller),
+		);
+
+		// send global info
+		Network.entities.ent_Character.info_changed.PostAllClients(
+			[Controller.Info.Controlling.Instance],
+			'Replicated',
+			GenerateCharacterInfo(Controller),
 		);
 	});
-	ent_player_controller.signal_died.Connect(previous_owner => {
-		if (previous_owner === undefined) return;
-		const controller_owner = Services.Players.GetPlayerByUserId(previous_owner);
-		if (!controller_owner) return;
+	Controller.signal_died.Connect(Monitor => {
+		if (Monitor === undefined) return;
 
 		Network.entities.ent_Character.info_changed.PostClient(
-			[controller_owner],
-			char_InfoFromController(ent_player_controller),
+			[Monitor.Instance],
+			'Local',
+			GenerateCharacterInfo(Controller),
 		);
 	});
 }
-created_controllers.sort((a, b) => {
-	return a.ent_id > b.ent_id;
+created_CharacterControllers.sort((a, b) => {
+	return a.id > b.id;
 });
 
-// client respawn request
-respawn_req.callback = player_data => {
-	const player_instance = Services.Players.GetPlayerByUserId(player_data.UserId);
-	if (player_instance === undefined) return;
+//ANCHOR Character respawning
+Network.CharacterRespawn.OnServerInvoke = Player => {
+	const MonitorData = Signals.GetDataFromPlayerId.Call(Player.UserId).await()[1] as PlayerMonitor | undefined;
+	if (!MonitorData) return;
 
 	// search if the player already has a controller assigned
 	if (
-		created_controllers.find(controller => {
-			return controller.ent_owner === player_data.UserId;
+		created_CharacterControllers.find(controller => {
+			return controller.Info.Controlling === MonitorData;
 		})
 	)
-		return false;
-
-	// loop search until a controller is avaiable
-	// const controller = created_controllers.find(controller => {return controller.ent_owner === undefined});
-	// if (controller === undefined) return false;
-	// controller.Kill();
-	// controller.Spawn(player_data.UserId);
+		return 'Already has an controller assigned';
 
 	let controller: CharacterController | undefined;
 	while (controller === undefined)
-		for (let index = 0; index < created_controllers.size(); index++) {
-			const element = created_controllers[index];
-			if (element.ent_owner === undefined) controller = element;
+		for (let index = 0; index < created_CharacterControllers.size(); index++) {
+			const element = created_CharacterControllers[index];
+			if (element.Info.Controlling === undefined) controller = element;
 			if (controller !== undefined) break;
 		}
 
 	controller.Kill();
-	controller.Spawn(player_data.UserId);
+	controller.Unassign();
+	controller.Assign(MonitorData);
+	controller.collisionbox.SetNetworkOwner(MonitorData.Instance); // needs to be set manually for some reason
+	controller.Spawn();
 
-	// Random spawn location
-	const spawn_children = Folders.Map.func_spawn.GetChildren();
-	let Spawn_Location = new CFrame(0, 10, 0);
-	if (!spawn_children.isEmpty()) {
-		let Found = false;
-		do {
-			const result = spawn_children[math.random(0, spawn_children.size())];
-			if (!result || !result.IsA('BasePart')) continue;
-
-			Spawn_Location = result.CFrame;
-			Found = true;
-			break;
-		} while (Found === false);
-	}
-
-	controller.ent_collisionbox.CFrame = Spawn_Location;
-	controller.ent_collisionbox.Anchored = false;
-	controller.ent_collisionbox.SetNetworkOwner(player_instance);
-
-	Network.entities.ent_Character.info_changed.PostClient([player_instance], char_InfoFromController(controller));
+	Network.entities.ent_Character.info_changed.PostClient([Player], 'Local', GenerateCharacterInfo(controller));
 
 	//TODO alert all players when a new entity spawns in
 };
